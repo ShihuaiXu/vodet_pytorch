9de84630bd4475b208ccd328f9971caf56cf34ed
shihuai.xu <shihuai.xu@enjoymove.cn>
2023-12-08 10:17:50 +0800

    chore: vis_better

--- model_tools/vodet_show_results_tool.py
+++ model_tools/vodet_show_results_tool.py
@@ -51,26 +51,27 @@ def multi_show_result(data_args, image, results, bbox_thresh, pt_thresh, show_tx
         for result in result_temp[0]:
             bbox, conf, hps_merge = result[:4], result[4], result[5:]
             if conf >= bbox_thresh:
-                bbox = np.array(bbox, dtype=np.int32)
-                cv2.rectangle(image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), c, 1)
-                if show_txt:
-                    txt = '{}'.format(data_args['class_names'][i])
-                    num_x, num_y = str(conf).split('.')
-                    txt = txt + '_' + str(num_x) + '.' + str(num_y[0])
-                    cat_size = cv2.getTextSize(txt, font, 0.5, 1)[0]
+                txt = '{}'.format(data_args['class_names'][i])
+                num_x, num_y = str(conf).split('.')
+                txt = txt + '_' + str(num_x) + '.' + str(num_y[0])
+                cat_size = cv2.getTextSize(txt, font, 0.5, 1)[0]
 
-                    cv2.rectangle(image, (bbox[0], bbox[1] - cat_size[1] - 2), (bbox[0] + cat_size[0], bbox[1] - 2), c,
-                                  -1)
-                    cv2.putText(image, txt, (bbox[0], bbox[1] - 2), font, 0.5, (0, 0, 0), thickness=1,
-                                lineType=cv2.LINE_AA)
+                bbox = np.array(bbox, dtype=np.int32)
+                if i not in data_args['pts_center_index']:
+                    cv2.rectangle(image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), c, 1)
+                    cv2.rectangle(image, (bbox[0], bbox[1] - cat_size[1] - 2), (bbox[0] + cat_size[0], bbox[1] - 2), c, -1)
+                    cv2.putText(image, txt, (bbox[0], bbox[1] - 2), font, 0.5, (0, 0, 0), thickness=1, lineType=cv2.LINE_AA)
+                else:
+                    cv2.putText(image, txt, (bbox[0], bbox[1] - 50), font, 0.5, (0, 0, 0), thickness=1, lineType=cv2.LINE_AA)
                 # only while bbox > thresh, pts will be output
                 # pts conf, start from 7, bbox(4), score(1), hps_merge(x1, y1, conf1, x2, y2, conf2, ...)
                 for j in range(0, hps_merge.size, 3):
                     if hps_merge[j + 2] >= pt_thresh:
                         cv2.circle(image, (int(hps_merge[j]), int(hps_merge[j + 1])), 2, c, -1)
-                        cv2.putText(image, str(j // 3) + ': ' + str(hps_merge[j + 2])[:3],
-                                    (int(hps_merge[j]), int(hps_merge[j + 1])), font, 0.5, c, thickness=1,
-                                    lineType=cv2.LINE_AA)
+                        cv2.putText(image, str(j // 3), (int(hps_merge[j]), int(hps_merge[j + 1])), font, 0.3, c,
+                                    thickness=1, lineType=cv2.LINE_AA)
+                        cv2.putText(image, str(hps_merge[j + 2])[:3], (int(hps_merge[j]), int(hps_merge[j + 1])-10),
+                                    font, 0.3, c, thickness=1, lineType=cv2.LINE_AA)
 
 
 def show_results(data_args, out_full_path, image, ctdet_results, multi_results, bbox_thresh=0.5, pt_thresh=0.3,
